f82b72b883abb37ca45862f854a615f895cff3ac
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-18 05:27:24 -0600

    chore: install react router

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Header />
+        <App />
+      </div>
+    ),
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
