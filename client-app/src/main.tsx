22bb5237e617e018979c5b3337a9b4854129d14d
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-18 06:03:13 -0600

    feature: create profile route

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import Root from "./routes/root.tsx";
+import Profile from "./routes/profile.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import "./index.css";
@@ -11,15 +12,19 @@ const router = createBrowserRouter([
     path: "/",
     element: (
       <div>
-        <Header />
-        <App />
+        <Root />
       </div>
     ),
     errorElement: <ErrorPage />,
   },
+  {
+    path: "profile/:id",
+    element: <Profile />,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- client-app/src/routes/root.tsx
+++ client-app/src/routes/root.tsx
@@ -1,4 +1,4 @@
-import "@App.css";
+import "../App.css";
 import Home from "@/components/Home";
 
 export default function Root() {
