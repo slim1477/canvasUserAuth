4070ee6afe1e7242dcc5e943326f5583cb8050d1
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-18 19:12:09 -0600

    feat: profile layout

--- client-app/src/routes/profile.tsx
+++ client-app/src/routes/profile.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 export default function Profile() {
   return (
-    <section className="mt-5 px-3 grid gap-5 lg:grid-rows-3 lg:grid-flow-col ">
+    <section className="mt-5 px-3 grid gap-5 lg:grid-cols-[30%_auto] lg:grid-rows-3 lg:p-[15px] lg:grid-flow-col ">
       <div className="rounded-lg lg:row-span-2 lg:col-start-1">
         <div className="flex flex-col gap-5 rounded-lg lg:col-start-1">
           <div className="h-[100px] shadow-md bg-background rounded-lg">
