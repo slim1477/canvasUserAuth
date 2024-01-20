95fe2c4c3a9e540f1142bf7da1e7891926012935
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-20 09:57:03 -0600

    fix closing div tag

--- client-app/src/routes/profile.tsx
+++ client-app/src/routes/profile.tsx
@@ -6,8 +6,6 @@ export default function Profile() {
     <section className="mt-5 px-3 grid gap-5 lg:grid-cols-[30%_auto] lg:grid-rows-3 lg:p-[15px] lg:grid-flow-col xl:grid-cols-[27%_auto]">
       <div className="rounded-lg lg:row-span-2 lg:col-start-1">
         <div className="flex flex-col gap-5 rounded-lg lg:col-start-1">
-          <div className="h-[350px] shadow-md bg-background rounded-lg">
-            Member Profile
           <div className="shadow-md bg-background rounded-lg">
             <ProfileDetails
               memberName="James Doe"
