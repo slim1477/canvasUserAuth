7deb9e7fc86b1f505070eaa41ab2354b11e3fb6e
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-20 09:45:46 -0600

    chore: build profile details

--- client-app/src/routes/profile.tsx
+++ client-app/src/routes/profile.tsx
@@ -1,10 +1,19 @@
+import { ProfileDetails } from "@/components/Profile/profileDetails";
+
 export default function Profile() {
   return (
-    <section className="mt-5 px-3 grid gap-5 lg:grid-cols-[30%_auto] lg:grid-rows-3 lg:p-[15px] lg:grid-flow-col ">
+    <section className="mt-5 px-3 grid gap-5 lg:grid-cols-[30%_auto] lg:grid-rows-3 lg:p-[15px] lg:grid-flow-col xl:grid-cols-[27%_auto]">
       <div className="rounded-lg lg:row-span-2 lg:col-start-1">
         <div className="flex flex-col gap-5 rounded-lg lg:col-start-1">
-          <div className="h-[100px] shadow-md bg-background rounded-lg">
-            Member Profile
+          <div className="shadow-md bg-background rounded-lg">
+            <ProfileDetails
+              memberName="James Doe"
+              memberNumber="17845500"
+              dateOfBirth="12/21/1939"
+              phone="+1(505)816-4459"
+              email="James.doe@gmail.com"
+              address="2-54 erkensel rd, Winnipeg MB, R1Y 2F9"
+            />
           </div>
           <div className="h-[40px] shadow-md bg-background rounded-2xl">
             Verification Button
