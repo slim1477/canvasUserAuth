9cdb3b3f63c9432d1392e9ead336ae32382fcb8b
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-20 09:53:37 -0600

    import profile details

--- client-app/src/routes/profile.tsx
+++ client-app/src/routes/profile.tsx
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-import { ProfileDetails } from "@/components/Profile/profileDetails";
-=======
 import { VerifyButton } from "@/components/verification/VerifyButton";
-
->>>>>>> feature/verification-ui
+import { ProfileDetails } from "@/components/Profile/ProfileDetails";
 
 export default function Profile() {
   return (
     <section className="mt-5 px-3 grid gap-5 lg:grid-cols-[30%_auto] lg:grid-rows-3 lg:p-[15px] lg:grid-flow-col xl:grid-cols-[27%_auto]">
       <div className="rounded-lg lg:row-span-2 lg:col-start-1">
         <div className="flex flex-col gap-5 rounded-lg lg:col-start-1">
-<<<<<<< HEAD
+          <div className="h-[350px] shadow-md bg-background rounded-lg">
+            Member Profile
           <div className="shadow-md bg-background rounded-lg">
             <ProfileDetails
               memberName="James Doe"
@@ -20,10 +17,6 @@ export default function Profile() {
               email="James.doe@gmail.com"
               address="2-54 erkensel rd, Winnipeg MB, R1Y 2F9"
             />
-=======
-          <div className="h-[350px] shadow-md bg-background rounded-lg">
-            Member Profile
->>>>>>> feature/verification-ui
           </div>
           <div className="h-[50px] lg:w-[50%] lg:flex lg:justify-end">
             <VerifyButton />
