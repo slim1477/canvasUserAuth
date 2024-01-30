eefb2cf77a1f51cd4910345e3308aedae67b25b5
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-29 21:00:31 -0600

    refactor: result list types

--- client-app/src/components/Home/SearchResultList.tsx
+++ client-app/src/components/Home/SearchResultList.tsx
@@ -1,25 +1,28 @@
 import { SearchResult } from "./SearchResult";
 
 interface User {
-  name: string;
+  firstName: string;
+  lastName: string;
   id: number;
   username: string;
   email: string;
   address: {};
-  phone: string;
+  phoneNumber: string;
 }
 export const SearchResultList = ({ results }: { results: User[] }) => {
+  console.log(results);
   return (
     <div className="border shadow-lg transition-all duration-300 delay-300">
       {results.length > 0 &&
         results.map((member) => {
+          const fullName = member.firstName + " " + member.lastName;
           return (
             <a href="profile/1">
               <SearchResult
-                key={member.email}
+                key={member.id}
                 email={member.email}
-                memberName={member.name}
-                phone={member.phone}
+                memberName={fullName}
+                phone={member.phoneNumber}
               />
             </a>
           );
