9f71eaa169ae4900e09c20dce71eaba5b10599d0
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-02-01 07:30:03 -0600

    bug: fix key prop

--- client-app/src/components/Home/SearchResultList.tsx
+++ client-app/src/components/Home/SearchResultList.tsx
@@ -10,16 +10,14 @@ interface User {
   phoneNumber: string;
 }
 export const SearchResultList = ({ results }: { results: User[] }) => {
-  console.log(results);
   return (
     <div className="border shadow-lg transition-all duration-300 delay-300">
       {results.length > 0 &&
         results.map((member) => {
           const fullName = member.firstName + " " + member.lastName;
           return (
-            <a href="profile/1">
+            <a key={member.id} href="profile/1">
               <SearchResult
-                key={member.id}
                 email={member.email}
                 address={member.address}
                 memberName={fullName}
